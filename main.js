53c6027a9da2078f5ce76a3fe3fb53f543f6804a
ma-suwa <rspturn@gmail.com>
2025-04-19 20:48:24 +0900

    scroll処理

---
+++ main.js
@@ -0,0 +1,17 @@
+  let lastScroll = 0;
+  const nav = document.querySelector('.bottom-nav');
+
+  window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+    console.log(currentScroll);
+
+    if (currentScroll > lastScroll && currentScroll > 100) {
+      // 下にスクロールしたら隠す
+      nav.classList.add('hidden');
+    } else {
+      // 上にスクロールしたら表示
+      nav.classList.remove('hidden');
+    }
+
+    lastScroll = currentScroll;
+  });
